8e958cdd818654ea81ae1a04809ce6224b512ce3
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2025-01-10 23:10:01 +0700

    Create index.js

---
+++ Pokedex/index.js
@@ -0,0 +1,44 @@
+document.getElementById("searchBar").addEventListener("submit", function(event) {
+    event.preventDefault();
+    fetchData(); 
+});
+
+async function fetchData(){
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+        
+        const data = await response.json();
+        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+        const nameElement = document.getElementById("pokemonNameDisplay");
+        nameElement.textContent = data.name;
+
+        const types = data.types.map(type => type.type.name).join(", ");
+        const typeElement = document.getElementById("pokemonTypeDisplay");
+        typeElement.textContent = `POKEMON TYPE: ${types}`;
+
+        const baseStatElement = document.getElementById("baseStat");
+        baseStatElement.textContent = "Base Stat";
+
+        const stats = data.stats.map(stat => {
+            return `${stat.stat.name}: ${stat.base_stat}`;
+        });
+        const statsElement = document.getElementById("pokemonStats");
+        statsElement.innerHTML = stats.map(stat => `<p>${stat}</p>`).join("");
+
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
